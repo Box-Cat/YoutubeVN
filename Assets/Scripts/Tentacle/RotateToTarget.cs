60a97c80923ba3206ed63e131b65edf15d9ea3b2
Box-Cat <pjh211pjh@gmail.com>
2025-05-01 03:47:07 +0900

    촉수 코드 일원화

--- Assets/Scripts/Tentacle/RotateToTarget.cs
+++ Assets/Scripts/Tentacle/RotateToTarget.cs
@@ -8,16 +8,17 @@ public class RotateToTarget : MonoBehaviour
     public float rotationSpeed;
     private Vector2 direction;
     public float moveSpeed;
+    public Vector2 offsetFromMouse = Vector2.zero;
 
     void Update()
     {
-        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        direction = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) + offsetFromMouse - (Vector2)transform.position;
         float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle,Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
 
 
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 cursorPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) + offsetFromMouse;
         transform.position = Vector2.MoveTowards(transform.position,cursorPos,moveSpeed * Time.deltaTime);
     }
 }
